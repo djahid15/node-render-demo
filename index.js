fa0c9806f219117333b65c02916e07dd4839f0d8
djahid15 <djoumitadjeddine19@gmail.com>
2025-10-30 16:56:24 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+res.send("■ Bonjour depuis Render !");
+});
+app.listen(PORT, () => {
+console.log(`Serveur lancé sur le port ${PORT}`);
+});
